7acb4a2ebeb2029adb8f714470b2eb6b5eb1fd74
shotarai <second.arasho@outlook.jp>
2023-07-14 02:23:02 +0900

    add font, change some design

--- frontend_project/src/pages/MakePage.jsx
+++ frontend_project/src/pages/MakePage.jsx
@@ -8,6 +8,7 @@ import { getDownloadURL } from "firebase/storage";
 import CircularProgress from "@mui/material/CircularProgress";
 import HandleSendData from "../components/sendNewMonster";
 import GetMonsterId from "../components/GetMonsterId";
+import space_img from "../assets/space.png";
 
 const MakePage = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -154,137 +155,145 @@ const MakePage = () => {
   // };
 
   return (
-    <>
-      <div className="flex items-center justify-center h-screen w-screen">
-        <form onSubmit={handleModalOpen}>
-          <h1 className="text-center text-2xl mb-4">
-            キャラの情報を登録してください。
-          </h1>
-          <div className="flex items-center">
-            <div className="">
-              <div className="flex justify-center">
-                <div className="mb-4 bg-slate-400">
-                  <label htmlFor="file" className="block">
-                    Select Image (PNG only):
-                  </label>
-                  <input
-                    type="file"
-                    id="file"
-                    accept="image/png"
-                    onChange={handleFileSelect}
-                    className="w-full"
-                  />
-                </div>
-              </div>
-              {selectedFile && (
-                <div className="border-4">
-                  <img
-                    src={selectedFile}
-                    alt="Selected"
-                    className="max-w-full"
-                  />
-                </div>
-              )}
-            </div>
-            <div className="">
-              <div className="mb-4">
-                <label htmlFor="name" className="block">
-                  名前
+    <div
+      className="flex flex-col items-center w-screen h-screen justify-end"
+      style={{ backgroundImage: `url(${space_img})`, backgroundSize: "cover" }}
+    >
+    <h2 className="text-center">
+    <div className="flex items-center justify-center h-screen w-screen">
+      <form onSubmit={handleModalOpen}>
+        <h2 className="text-center text-2xl text-white bg-gray-900 mb-4">
+          キャラの情報を登録してください。
+        </h2>
+        <div className="flex items-center">
+          <div className="">
+            <div className="flex justify-center">
+              <div className="mb-4 bg-slate-400">
+                <label htmlFor="file" className="block">
+                  Select Image (PNG only):
                 </label>
+                <input
+                  type="file"
+                  id="file"
+                  accept="image/png"
+                  onChange={handleFileSelect}
+                  className="w-full"
+                />
+              </div>
+            </div>
+            {selectedFile && (
+              <div className="border-4">
+                <img
+                  src={selectedFile}
+                  alt="Selected"
+                  className="max-w-full"
+                />
+              </div>
+            )}
+          </div>
+          <div className="">
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-xl font-bold mb-2">
+                名前
+              </label>
+              <div className="relative">
                 <input
                   type="text"
                   id="name"
                   value={name}
                   onChange={handleNameChange}
-                  className="w-full border border-gray-300 rounded px-2 py-1"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="name" className="block">
-                  年齢
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  value={age}
-                  onChange={handleAgeChange}
-                  className="w-full border border-gray-300 rounded px-2 py-1"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="gender" className="block">
-                  性別
-                </label>
-                <input
-                  type="text"
-                  id="gender"
-                  value={gender}
-                  onChange={handleGenderChange}
-                  className="w-full border border-gray-300 rounded px-2 py-1"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="race" className="block">
-                  種族
-                </label>
-                <input
-                  type="text"
-                  id="race"
-                  value={race}
-                  onChange={handleRaceChange}
-                  className="w-full border border-gray-300 rounded px-2 py-1"
-                />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="hobby" className="block">
-                  趣味
-                </label>
-                <input
-                  type="text"
-                  id="hobby"
-                  value={hobby}
-                  onChange={handleHobbyChange}
-                  className="w-full border border-gray-300 rounded px-2 py-1"
+                  className="w-full border-4 border-gold bg-gray-800 text-white pl-4 py-1 pr-12 text-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  placeholder="入力してください"
                 />
               </div>
             </div>
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white rounded py-2"
-          >
-            登録
-          </button>
-        </form>
-        {showModal && (
-          <div className="bg-gray-600 bg-opacity-50 fixed top-0 left-0 w-full h-screen flex justify-center items-center">
-            <div className="bg-white p-4 rounded">
-              <h3 className="text-xl mb-2">Confirmation Dialog</h3>
-              <p>このキャラクターを登録しますか？</p>
-              <div className="mt-4 flex justify-end">
-                <button
-                  onClick={handleModalClose}
-                  className="mr-2 bg-gray-500 text-white px-4 py-2 rounded"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={handleRegister}
-                  className="bg-blue-500 text-white px-4 py-2 rounded"
-                >
-                  OK!
-                </button>
-              </div>
-              {isLoading && <CircularProgress />}
+            <div className="mb-4">
+              <label htmlFor="name" className="block">
+                年齢
+              </label>
+              <input
+                type="text"
+                id="name"
+                value={age}
+                onChange={handleAgeChange}
+                className="w-full border border-gray-300 rounded px-2 py-1"
+              />
             </div>
-            {/* <div
-              className=" absolute top-0 left-0 w-full h-screen"
-              onClick={handleModalClose}
-            /> */}
+            <div className="mb-4">
+              <label htmlFor="gender" className="block">
+                性別
+              </label>
+              <input
+                type="text"
+                id="gender"
+                value={gender}
+                onChange={handleGenderChange}
+                className="w-full border border-gray-300 rounded px-2 py-1"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="race" className="block">
+                種族
+              </label>
+              <input
+                type="text"
+                id="race"
+                value={race}
+                onChange={handleRaceChange}
+                className="w-full border border-gray-300 rounded px-2 py-1"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="hobby" className="block">
+                趣味
+              </label>
+              <input
+                type="text"
+                id="hobby"
+                value={hobby}
+                onChange={handleHobbyChange}
+                className="w-full border border-gray-300 rounded px-2 py-1"
+              />
+            </div>
           </div>
-        )}
-      </div>
-    </>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-gray-900 text-white rounded-lg py-2 hover:bg-gray-700 transition duration-300"
+        >
+          登録
+        </button>
+      </form>
+      {showModal && (
+        <div className="bg-gray-600 bg-opacity-50 fixed top-0 left-0 w-full h-screen flex justify-center items-center">
+          <div className="bg-white p-4 rounded">
+            <h3 className="text-xl mb-2">Confirmation Dialog</h3>
+            <p>このキャラクターを登録しますか？</p>
+            <div className="mt-4 flex justify-end">
+              <button
+                onClick={handleModalClose}
+                className="mr-2 bg-gray-500 text-white px-4 py-2 rounded"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleRegister}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                OK!
+              </button>
+            </div>
+            {isLoading && <CircularProgress />}
+          </div>
+          {/* <div
+            className=" absolute top-0 left-0 w-full h-screen"
+            onClick={handleModalClose}
+          /> */}
+        </div>
+      )}
+    </div>
+    </h2>
+    </div>
   );
 };
 
