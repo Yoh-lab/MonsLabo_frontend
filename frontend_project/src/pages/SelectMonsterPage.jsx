6b16049b74a487545f94a6f7207d72dd63062861
sibainuyou <otsuka@m.cs.osakafu-u.ac.jp>
2023-07-14 04:42:39 +0900

    SlectMonseterPageを編集中

--- frontend_project/src/pages/SelectMonsterPage.jsx
+++ frontend_project/src/pages/SelectMonsterPage.jsx
@@ -5,8 +5,8 @@ import HandleGetData from "../components/GetAllMonsterData";
 
 const SelectMonsterPage = () => {
   const [getData, setGetData] = useState(null);
-  // const [showModal, setShowModal] = useState(false);
-  // const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchDataAndSetData = async () => {
@@ -29,41 +29,40 @@ const SelectMonsterPage = () => {
     navigate("/selectMode");
   };
 
+  
+  const [selectedId, setSelectedId] = useState(null);
+    const handleModalOpen = (id) => {
+    setSelectedId(id);
+    setShowModal(true);
+  };
 
-  //   const handleModalOpen = (id) => {
-  //   setSelectedId(id);
-  //   setShowModal(true);
-  // };
-
-  // const handleModalClose = () => {
-  //   setShowModal(false);
-  // };  
-
-  // const [slectedId, setSelectedId] = useState(0);
-  // const handleRegister = async (event) => {
-  //   event.preventDefault();
-  //   setIsLoading(true);
-  //   try {
-  //     navigate("/talk", {
-  //       state: {
-  //         selectedFile: getData[slectedId].image_url,
-  //         monsterId: String(slectedId),
-  //         name: getData[slectedId].name,
-  //         age: String(getData[slectedId].age),
-  //         gender: getData[slectedId].image_url,
-  //         hobby: getData[slectedId].hobby,
-  //         race: getData[slectedId].race,
-  //         _logInput: getData[slectedId]._logInput,
-  //         _logOutput: getData[slectedId]._logOutput,
-  //         num_response: String(getData[slectedId].num_response),
-  //         image_url: getData[slectedId].image_url,
-  //       },
-  //     });
-  //   } catch (error) {
-  //     console.log("画像のURL取得時にエラーが発生しました。", error);
-  //   }
-  //   setIsLoading(false);
-  // };
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      navigate("/talk", {
+        state: {
+          selectedFile: getData[selectedId].image_url,
+          monsterId: String(selectedId),
+          name: getData[selectedId].name,
+          age: String(getData[selectedId].age),
+          gender: getData[selectedId].image_url,
+          hobby: getData[selectedId].hobby,
+          race: getData[selectedId].race,
+          _logInput: getData[selectedId]._logInput,
+          _logOutput: getData[selectedId]._logOutput,
+          num_response: String(getData[selectedId].num_response),
+          image_url: getData[selectedId].image_url,
+        },
+      });
+    } catch (error) {
+      console.log("画像のURL取得時にエラーが発生しました。", error);
+    }
+    setIsLoading(false);
+  };
 
   if (!getData) {
     return (
@@ -76,11 +75,6 @@ const SelectMonsterPage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <h1 className="text-4xl mb-8">モンスターを選んでください</h1>
-      {/* <button onClick={HandleGetData}>Firestoreからデータを受け取る</button>  
-      <h1>{userId}</h1>
-      <h1>{errorMessages}</h1>
-      <h1>{responseMessages}</h1>
-      {isLoading && <CircularProgress />} */}
       <div className="flex justify-center">
         {Object.keys(getData).map((key) => (
           <div key={key} className="">
@@ -89,7 +83,7 @@ const SelectMonsterPage = () => {
               alt=""
               style={{ width: "200px", height: "auto" }}
             />
-            {/* <button onClick={handleModalOpen(key)}>{getData[key].name}</button> */}
+            <button onClick={handleModalOpen(key)}>{getData[key].name}</button>
           </div>
         ))}
       </div>
@@ -106,9 +100,9 @@ const SelectMonsterPage = () => {
         戻る
       </button>
 
-      {/* {showModal && ( */}
-        {/* <div className="bg-gray-600 bg-opacity-50 fixed top-0 left-0 w-full h-screen flex justify-center items-center"> */}
-          {/* <div className="bg-white p-4 rounded">
+      {showModal && (
+        <div className="bg-gray-600 bg-opacity-50 fixed top-0 left-0 w-full h-screen flex justify-center items-center">
+          <div className="bg-white p-4 rounded">
             <h3 className="text-xl mb-2">Confirmation Dialog</h3>
             <p>このモンスターで遊ぶ？</p>
             <div className="mt-4 flex justify-end">
@@ -126,14 +120,13 @@ const SelectMonsterPage = () => {
               </button>
             </div>
             {isLoading && <CircularProgress />}
-          </div> */}
-          {/* <div
-              className=" absolute top-0 left-0 w-full h-screen"
-              onClick={handleModalClose}
-            /> */}
-        {/* </div> */}
-      {/* )} */}
-      
+          </div>
+          <div
+            className=" absolute top-0 left-0 w-full h-screen"
+            onClick={handleModalClose}
+          />
+        </div>
+      )}
     </div>
   );
 };
