915e1c15495469465e8a5e58a3ff85614ec8735a
sibainuyou <otsuka@m.cs.osakafu-u.ac.jp>
2023-07-14 14:37:32 +0900

    画面右上にボタンを用意し、押したらサインアウトできるようにした。ここのUIを変更して設定ボタンに置き換える

---
+++ frontend_project/src/components/SettingButton.jsx
@@ -0,0 +1,13 @@
+import SignOut from "../components/SignOut";
+
+const SettingButton = () => {
+  return (
+    <div>
+      <div className="fixed top-4 right-4 z-50">
+      <SignOut />
+      </div>
+    </div>
+  );
+};
+
+export default SettingButton;
--- frontend_project/src/pages/SelectModePage.jsx
+++ frontend_project/src/pages/SelectModePage.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import paint_img from "../assets/lab_sample.jpeg";
 import { useState, useEffect } from "react";
 import GetMonsterCount from "../components/GetMonsterCount";
-import SignOut from "../components/SignOut";
+import SettingButton from "../components/SettingButton";
 
 const SelectModePage = () => {
   const navigate = useNavigate();
@@ -37,37 +37,39 @@ const SelectModePage = () => {
     );
   }
   return (
-    <div
-      className="flex flex-col items-center justify-center w-screen h-screen"
-      style={{
-        backgroundImage: `url(${paint_img})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="w-700 h-375 text-white flex flex-col w-3/5 justify-center items-center gap-20 rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-10 bg-green-400 shadow-md border-2 border-white-40 border-b-2 border-r-2">
-        <h1 className="text-4xl mb-8">モンスターゲーム</h1>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-          onClick={handleCreateMonster}
-        >
-          モンスターを作成
-        </button>
-
-        {getData ? (
+    
+    <div>
+      <SettingButton />
+      <div
+        className="flex flex-col items-center justify-center w-screen h-screen"
+        style={{
+          backgroundImage: `url(${paint_img})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="w-700 h-375 text-white flex flex-col w-3/5 justify-center items-center gap-20 rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-10 bg-green-400 shadow-md border-2 border-white-40 border-b-2 border-r-2">
+          <h1 className="text-4xl mb-8">モンスターゲーム</h1>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleTalkToMonster}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+            onClick={handleCreateMonster}
           >
-            モンスターと会話
+            モンスターを作成
           </button>
-        ) : (// １体もモンスターがいない場合はモンスターと会話ボタンを薄くして、クリックできないようにする
-          <button
-            className="bg-red-200 hover:bg-red-300 text-white font-bold py-2 px-4 rounded"
-          >
-            モンスターと会話
-          </button>
-        )}
-        <SignOut />
+          {getData >0 ? (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleTalkToMonster}
+            >
+              モンスターと会話
+            </button>
+          ) : (// １体もモンスターがいない場合はモンスターと会話ボタンを薄くして、クリックできないようにする
+            <button
+              className="bg-red-200 hover:bg-red-300 text-white font-bold py-2 px-4 rounded"
+            >
+              モンスターと会話
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
