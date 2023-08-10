48e021c9f7752fe6a99d8a387fce18968e6c303d
shotarai <second.arasho@outlook.jp>
2023-08-11 01:51:33 +0900

    some changes

--- frontend_project/src/pages/TalkPage.jsx
+++ frontend_project/src/pages/TalkPage.jsx
@@ -1,4 +1,4 @@
-import { useState, } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -22,13 +22,29 @@ const TalkPage = () => {
   const hobby = location.state?.hobby || null;
   const race = location.state?.race || null;
   const [_logInput, setLogInput] = useState(location.state?._logInput || null);
-  const [_logOutput, setLogOutput] = useState(location.state?._logOutput || null);
-  const [num_response, setNumResponse] = useState(location.state?.num_response || null);
+  const [_logOutput, setLogOutput] = useState(
+    location.state?._logOutput || null
+  );
+  const [num_response, setNumResponse] = useState(
+    location.state?.num_response || null
+  );
   const image_url = location.state?.image_url || null;
   const [new_num_response, setNewNumResponse] = useState(0);
 
   const handleInputChange = (e) => {
-    console.log(showModal, monsterId, name, age, gender, hobby, race, _logInput, _logOutput, num_response, image_url);
+    console.log(
+      showModal,
+      monsterId,
+      name,
+      age,
+      gender,
+      hobby,
+      race,
+      _logInput,
+      _logOutput,
+      num_response,
+      image_url
+    );
     setFormInput(e.target.value);
   };
 
@@ -62,13 +78,13 @@ const TalkPage = () => {
         monsterId: monsterId,
         _logInput: _logInput.concat([formInput]),
         _logOutput: _logOutput.concat([response.data]),
-        num_response: Number(Number(num_response)+1),
-      }
-      HandleUpdateData(NewData)
+        num_response: Number(Number(num_response) + 1),
+      };
+      HandleUpdateData(NewData);
       if (num_response == 4) {
-        setShowModal(true)
+        setShowModal(true);
         console.log("showModal", showModal);
-      }else{
+      } else {
         console.log(num_response);
       }
       setNumResponse(Number(num_response) + 1);
@@ -83,7 +99,6 @@ const TalkPage = () => {
     }
     setIsLoading(false);
   };
-
 
   const handleTalkSubmit = async (e) => {
     e.preventDefault();
@@ -107,9 +122,9 @@ const TalkPage = () => {
         monsterId: monsterId,
         _logInput: _logInput.concat([formInput]),
         _logOutput: _logOutput.concat([response.data]),
-        num_response: Number(Number(num_response)+1),
-      }
-      HandleUpdateData(NewData)
+        num_response: Number(Number(num_response) + 1),
+      };
+      HandleUpdateData(NewData);
       setNumResponse(Number(num_response) + 1);
       setNewNumResponse(Number(new_num_response) + 1);
       setLogInput(_logInput.concat([formInput]));
@@ -123,110 +138,124 @@ const TalkPage = () => {
     setIsLoading(false);
   };
 
-
   const handleModalClose = () => {
     setShowModal(false);
   };
-
 
   return (
     <div className="flash_back">
       <SettingButton />
       <div
         className="flex flex-col items-center w-screen h-screen justify-center"
-        style={{ backgroundImage: `url(${talk_img})`, backgroundSize: "cover" , backgroundPosition: "center" }}
+        style={{
+          backgroundImage: `url(${talk_img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <div className="border-2 bg-white w-1/3 h-2/5 flex justify-center items-center">
-          {image_url ? (
-            <img src={image_url} className="max-h-72" alt="Image" />
-          ) : (
-            <div className="text-center">
-              <img
-                src="none.png"
-                className="max-h-72"
-              />
-              <p>No Image</p>
+        <div className="back_sheet z-10 relative"></div>
+        <div className="flex flex-col justify-center items-center z-20 absolute top-50% left-50%">
+          <div className="border-2 bg-white flex justify-center items-center">
+            {image_url ? (
+              <img src={image_url} className="max-h-72" alt="Image" />
+            ) : (
+              <div className="text-center">
+                <img src="none.png" className="max-h-72" />
+                <p>No Image</p>
+              </div>
+            )}
+          </div>
+          <h2 className="pt-2">
+            <div className="mt-2 w-screen">
+              <textarea
+                className="w-3/5 h-40 px-4 py-2 dark:text-black border border-gray-300 rounded-lg bg-gradient-to-t from-gray-400 to-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                value={
+                  responseData ||
+                  "５回会話を行なって，モンスターの育成を行なってください．語尾を学習します(例： ござる)"
+                }
+                readOnly // 入力禁止にする
+              ></textarea>
             </div>
-          )}
-        </div>
-        <h2 className="pt-2">
-        <div className="mt-2 w-screen">
-          <textarea
-            className="w-3/5 h-40 px-4 py-2 dark:text-black border border-gray-300 rounded-lg bg-gradient-to-t from-gray-400 to-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            value={responseData || "５回会話を行なって，モンスターの育成を行なってください．"}
-            readOnly // 入力禁止にする
-          ></textarea>
-        </div>
-        </h2>
-        <div className="my-2  pt-4"></div> {/* 余白を追加 */}
-        {/* 会話のログが５往復以内なら、育成中の文字を追加 */}
-        <div className="relative w-3/5">
-        {num_response < 5 ? (
-          <h2 className="absolute top-0 left-0 bg-black text-white border-2 border-gray-300 rounded">
-            育成中（{num_response}/5回）
           </h2>
-        ) : (
-          <h2 className="absolute top-0 left-0 bg-black text-white border-2 border-gray-300 rounded">
-            育成完了
-          </h2>
-        )}
-        <h2>
-        <form className="my-4 flex items-center justify-center pt-2" onSubmit={isLoading == false ? (num_response < 5 ? handleTrainingSubmit : handleTalkSubmit) : null}>
-          <textarea
-            className="flex-grow dark:text-black bg-white h-10 px-4 py-2 border-2 border-gray-300 mr-2 pl-8"
-            placeholder="例）こんにちは"
-            value={formInput}
-            onChange={handleInputChange}
-          ></textarea>
-          <button
-            type="submit"
-            className="px-4 py-2 text-white bg-black b-fuchsia-500 border-2 flex items-center"
-          >
-            <h2 className="mr-2">送信</h2>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-              />
-            </svg>
-          </button>
-          {isLoading && <CircularProgress />}
-        </form>
-        </h2>
-      </div>
+          <div className="my-2  pt-4"></div> {/* 余白を追加 */}
+          {/* 会話のログが５往復以内なら、育成中の文字を追加 */}
+          <div className="relative w-3/5">
+            {num_response < 5 ? (
+              <h2 className="absolute top-0 left-0 bg-black text-white border-2 border-gray-300 rounded">
+                育成中（{num_response}/5回）
+              </h2>
+            ) : (
+              <h2 className="absolute top-0 left-0 bg-black text-white border-2 border-gray-300 rounded">
+                育成完了
+              </h2>
+            )}
+            <h2>
+              <form
+                className="my-4 flex items-center justify-center pt-2"
+                onSubmit={
+                  isLoading == false
+                    ? num_response < 5
+                      ? handleTrainingSubmit
+                      : handleTalkSubmit
+                    : null
+                }
+              >
+                <textarea
+                  className="flex-grow dark:text-black bg-white h-10 px-4 py-2 border-2 border-gray-300 mr-2 pl-8"
+                  placeholder="例）こんにちは"
+                  value={formInput}
+                  onChange={handleInputChange}
+                ></textarea>
+                <button
+                  type="submit"
+                  className="px-4 py-2 text-white bg-black b-fuchsia-500 border-2 flex items-center"
+                >
+                  <h2 className="mr-2">送信</h2>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                    />
+                  </svg>
+                </button>
+                {isLoading && <CircularProgress />}
+              </form>
+            </h2>
+          </div>
+        </div>
       </div>
 
       {showModal && (
-          <div className="bg-gray-60 fixed top-0 left-0 w-full h-screen flex justify-center items-center">
-            <div className="bg-black p-4 rounded border-2 border-yellow-400">
-              <h2 className="text-xl text-white mb-2">育成完了！！</h2>
-              <h2>モンスターとの会話を楽しんでください！</h2>
-              <div className="mt-4 flex justify-end">
-                <h2>
+        <div className="bg-gray-60 fixed top-0 left-0 w-full h-screen flex justify-center items-center">
+          <div className="bg-black p-4 rounded border-2 border-yellow-400">
+            <h2 className="text-xl text-white mb-2">育成完了！！</h2>
+            <h2>モンスターとの会話を楽しんでください！</h2>
+            <div className="mt-4 flex justify-end">
+              <h2>
                 <button
                   onClick={handleModalClose}
                   className="mr-2 bg-black text-white px-4 py-2 rounded"
                 >
                   OK!
                 </button>
-                </h2>
-              </div>
-              {isLoading && <CircularProgress />}
+              </h2>
             </div>
-            {/* <div
+            {isLoading && <CircularProgress />}
+          </div>
+          {/* <div
               className=" absolute top-0 left-0 w-full h-screen"
               onClick={handleModalClose}
             /> */}
-          </div>
-        )}
+        </div>
+      )}
     </div>
   );
 };
